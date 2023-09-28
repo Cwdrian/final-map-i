e5ab6e96775509635699fd889570eadc50200159
Cwdrian <xcharlesdrian@gmail.com>
2023-09-27 21:05:49 -0400

    attempt

--- index.js
+++ index.js
@@ -1,59 +1,44 @@
 //api key1 user location fsq3ci0ruJBDLJjjrt0Gs6k1QbXF760Tk8QqOAZ2MUiSq6c=
-//api key2 locations fsq359ekcA18E4r5ucGwxI18STvGRVqdzExKo2YsFZMZ4/I=
 
 document.addEventListener("DOMContentLoaded", function () {
     // Create a Leaflet map
     var map = L.map("map");
 
-    // OpenStreetMap tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // OpenStreetMap
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Function to get the user's location and set the map view
     function getUserLocation() {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 var userLat = position.coords.latitude;
                 var userLng = position.coords.longitude;
 
-                // Set the map view to the user's location
+                // Set the map view to users location
                 map.setView([userLat, userLng], 13);
 
-                // Add a marker at the user's location with a popup
+                // Marker at the users location
                 var userMarker = L.marker([userLat, userLng]).addTo(map);
                 userMarker.bindPopup("You Are Here").openPopup();
 
-                // Fetch nearby businesses from Foursquare using API key1
-                var apiKey1 = "fsq3ci0ruJBDLJjjrt0Gs6k1QbXF760Tk8QqOAZ2MUiSq6c=";
-                var foursquareUrl = "https://api.foursquare.com/v2/venues/explore" +
-                    "?ll=" + userLat + "," + userLng +
-                    "&radius=1000" +  // You can adjust the radius as needed
-                    "&client_id=" + apiKey1 +
-                    "&client_secret=" + apiKey1 +  // Use the same API key for client secret
-                    "&v=20230901";  // Use the current date as the version
-
-                // Fetch and process nearby venues
-                fetch(foursquareUrl)
-                    .then(function (response) {
-                        return response.json();
-                    })
+                // Call the placeSearch function to fetch nearby coffee shops
+                placeSearch(userLat, userLng)
                     .then(function (data) {
-                        // Process the data and display nearby businesses on the map
-                        var venues = data.response.groups[0].items;
-                        venues.forEach(function (venue) {
-                            var lat = venue.venue.location.lat;
-                            var lng = venue.venue.location.lng;
-                            var name = venue.venue.name;
-                            var address = venue.venue.location.address;
+                        //display nearby coffee shops
+                        data.response.forEach(function (place) {
+                            var lat = place.location.lat;
+                            var lng = place.location.lng;
+                            var name = place.name;
+                            var address = place.location.address;
 
-                            // Add markers for nearby businesses
-                            var businessMarker = L.marker([lat, lng]).addTo(map);
-                            businessMarker.bindPopup(name + "<br>" + address);
+                            // Add markers for coffee shops
+                            var coffeeShopMarker = L.marker([lat, lng]).addTo(map);
+                            coffeeShopMarker.bindPopup(name + "<br>" + address);
                         });
                     })
                     .catch(function (error) {
-                        console.error("Error fetching nearby businesses:", error);
+                        console.error("Error fetching nearby coffee shops:", error);
                     });
             }, function (error) {
                 console.error("Error getting user location:", error);
@@ -63,8 +48,42 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Call the function to get the user's location and set the map view
+    // Call the function to get the users location and set the map view
     getUserLocation();
+
+    async function placeSearch(userLat, userLng) {
+        try {
+            const searchParams = new URLSearchParams({
+                query: 'coffee',
+                ll: `${userLat},${userLng}`,
+                open_now: 'false',
+                sort: 'DISTANCE'
+            });
+            
+            // CORS proxy URL
+            const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+            
+            //Foursquare API URL
+            const foursquareUrl = "https://api.foursquare.com/v3/places/search?${searchParams}";
+            
+            const results = await fetch(
+                corsProxyUrl + foursquareUrl,
+                {
+                    method: 'GET',
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: 'fsq3ci0ruJBDLJjjrt0Gs6k1QbXF760Tk8QqOAZ2MUiSq6c=', 
+                    }
+                }
+            );
+            const data = await results.json();
+            console.log(data); 
+            return data;
+        } catch (err) {
+            console.error("Error fetching nearby coffee shops:", err);
+        }
+    }
+    
 });
 
 
